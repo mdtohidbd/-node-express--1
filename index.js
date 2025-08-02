97055e093b2314a2a1d630f4878675ca7ee1a22d
mdtohidvai <mdtohid242024@gmail.com>
2025-08-02 19:08:56 +0600

    server uodate

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 
 app.use(cors());
+app.use(express.json());
 
 const users = [
     { id: 1, name: 'tohid', email: 'tohid2232@gmail.com' },
@@ -23,6 +24,11 @@ app.get('/users', (req, res) => {
 app.post('/users', (req, res) =>{
     console.log('post api hitting')
     console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser);
+
 })
 
 
